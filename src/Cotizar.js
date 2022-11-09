e550380cd459cdf9c1ec178abe6d1e2c68df51ac
DiegoQuintanaF <dquintana@utb.edu.co>
2022-11-09 17:57:03 -0500

    Agregado cotizar.js

---
+++ src/Cotizar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Cotizar extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Cotizar</h1>
+            </div>
+        )
+    }
+}
+
+export default Cotizar;
